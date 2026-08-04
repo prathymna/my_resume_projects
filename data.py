49bb7ae4a10a851c852e5555b6ee4a343bbda93c
Prathymna Reddy <prathymna2004@gmail.com>
2026-08-04 13:14:39 +0530

    Refactored ATS engine into modular data and main files

---
+++ data.py
@@ -0,0 +1,4 @@
+# data.py
+resume_text = "B.Tech Computer Science Engineering student from JJ College with experience in Python, HTML, and Git. Seeking a software developer role."
+
+required_skills = ["python", "git", "java", "html", "sql", "docker"]
--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print("AI resume scanner:system initializing...")
+# main.py
+import data  # This connects to your data.py file!
+
+print("Scanning Resume...\n")
+
+match_count = 0
+# Notice how we type 'data.' before the variable names now
+total_skills = len(data.required_skills)
+resume_lower = data.resume_text.lower()
+
+for skill in data.required_skills:
+    if skill in resume_lower:
+        print(f"✅ Found: {skill.capitalize()}")
+        match_count += 1
+    else:
+        print(f"❌ Missing: {skill.capitalize()}")
+
+raw_percentage = (match_count / total_skills) * 100
+final_score = round(raw_percentage)
+
+print("\n--- Final ATS Report ---")
+print(f"Match Score: {final_score}%")
+
+if final_score >= 50:
+    print("Status: 🟢 INTERVIEW SELECTED")
+else:
+    print("Status: 🔴 REJECTED")
